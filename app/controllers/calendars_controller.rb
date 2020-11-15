07bf4101b5e2b21234f5d1a3c859f89f057abf5d
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2020-11-15 11:33:17 +0900

    Revert "Merge pull request #8 from yusei0726/first_training"
    This reverts commit fddc417209bad08a8f8e797b2cc00fba0bc168be, reversing
    changes made to a7ed605d8a2120c4688d0068417012f94c611609.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,24 +18,24 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @get.day = Date.today
+    @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @get_week = []
+    @week_days = []
 
-    plans = Plan.where(date: @get_day..@get_day + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plan = plans.map do |plan|
-        today_plans.push(plan.plan) if plan.date == @get_date + x
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = {month: (@get_date + x).month, date: (@get_date+x).day, plans: today_plans}
-      @get_week.push(days)
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      @week_days.push(days)
     end
 
   end
